ffc23a91da793f0bc34aa6178acf4577f639d53d
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-21 11:31:19 +0700

    updated about external links

--- src/js/handler/link.js
+++ src/js/handler/link.js
@@ -1,6 +1,6 @@
 // open link in browser (not in electron app)
 document.body.addEventListener("click", (event) => {
-	if (event.target.tagName.toLowerCase() === "a" || event.target.tagName.toLowerCase() === "span") {
+	if (event.target.tagName.toLowerCase() === "a" || event.target.tagName.toLowerCase() === "span" || event.target.tagName.toLowerCase() === "img") {
 		if (event.target.id === "ext") {
 			event.preventDefault();
 			let target = event.target.href ? event.target.href : event.target.parentElement.href;
