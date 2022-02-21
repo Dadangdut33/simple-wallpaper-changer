59444ac027bde3a386c3cdd5ccc8964318e9e609
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-02-21 12:11:41 +0700

    main menu looks

--- src/js/pages_handler/main_menu.js
+++ src/js/pages_handler/main_menu.js
@@ -1,11 +1,28 @@
 const { ipcRenderer } = require("electron");
 const imgContainer = document.getElementById("img-container");
 
-const testVar = document.getElementById("test-var");
-// TODO: timer
+const timerQueue = document.getElementById("timer-queue");
+const toHHMMSS = function (string) {
+	var sec_num = parseInt(string, 10);
+	var hours = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - hours * 3600) / 60);
+	var seconds = sec_num - hours * 3600 - minutes * 60;
+
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	return hours + ":" + minutes + ":" + seconds;
+};
+
 ipcRenderer.send("start-timer", null);
 ipcRenderer.on("timer", (event, arg) => {
-	testVar.innerHTML = arg;
+	timerQueue.innerHTML = toHHMMSS(arg);
 });
 
 const loadImage_Queue = (images) => {
